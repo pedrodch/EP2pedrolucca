8f8d420d792e8e9a43ff33258062042f3aff58e7
Pedro Hun <pedrodch@al.insper.edu.br>
2021-05-10 23:09:40 -0300

    lista_movimentos/p

--- lista_movimentos.py
+++ lista_movimentos.py
@@ -1,3 +1,67 @@
+def cria_baralho():
+    import random
+    e1 = 'A♠'
+    c1 = 'A♥'
+    o1 = 'A♦'
+    p1 = 'A♣'
+    e2 = '2♠'
+    c2 = '2♥'
+    o2 = '2♦'
+    p2 = '2♣'
+    e3 = '3♠'
+    c3 = '3♥'
+    o3 = '3♦'
+    p3 = '3♣'
+    e4 = '4♠'
+    c4 = '4♥'
+    o4 = '4♦'
+    p4 = '4♣'
+    e5 = '5♠'
+    c5 = '5♥'
+    o5 = '5♦'
+    p5 = '5♣'
+    e6 = '6♠'
+    c6 = '6♥'
+    o6 = '6♦'
+    p6 = '6♣'
+    e7 = '7♠'
+    c7 = '7♥'
+    o7 = '7♦'
+    p7 = '7♣'
+    e8 = '8♠'
+    c8 = '8♥'
+    o8 = '8♦'
+    p8 = '8♣'
+    e9 = '9♠'
+    c9 = '9♥'
+    o9 = '9♦'
+    p9 = '9♣'
+    e10 = '10♠'
+    c10 = '10♥'
+    o10 = '10♦'
+    p10 = '10♣'
+    e11 = 'J♠'
+    c11 = 'J♥'
+    o11 = 'J♦'
+    p11 = 'J♣'
+    e12 = 'Q♠'
+    c12 = 'Q♥'
+    o12 = 'Q♦'
+    p12 = 'Q♣'
+    e13 = 'K♠'
+    c13 = 'K♥'
+    o13 = 'K♦'
+    p13 = 'K♣'
+    baralho= [e1, c1, o1, p1, e2, c2, o2, p2, e3, c3, o3, p3, e4, c4, o4, p4, e5, c5, o5, p5, e6, c6, o6, p6, e7, c7, o7, p7, e8, c8, o8, p8, e9, c9, o9, p9, e10, c10, o10, p10, e11, c11, o11, p11, e12, c12, o12, p12, e13, c13, o13, p13]
+    random.shuffle(baralho)
+    return baralho
+def extrai_naipe(carta):
+    return carta[-1]
+def extrai_valor(carta):
+    if len(carta) == 3:
+        return '10'
+    else:
+        return carta[0]
 def lista_movimentos_possiveis(baralho, posicao):
     naipe = extrai_naipe(baralho[posicao])
     numero = extrai_valor(baralho[posicao])
